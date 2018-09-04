13bf9944c4c6027a25fcd6965c8b6b4f5343b3b7
Gemmunard <34251020+Gemmunard@users.noreply.github.com>
2018-09-04 09:30:58 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,10 @@
 class Game{
    class Player;
    virtual void GamePlay() = 0;
-} 
+};
+
+class TickTaxkToe{
+   Player{
+      bool Turn(unsigned int l, unsingned int c)};
+   Player players[2]
+  };
