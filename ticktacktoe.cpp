0ef5c699361ea071f2e4ee4a1f70438446a837a8
Gemmunard <34251020+Gemmunard@users.noreply.github.com>
2018-09-05 21:25:06 +0300

    Update ticktacktoe.cpp

--- ticktacktoe.cpp
+++ ticktacktoe.cpp
@@ -1,33 +1,41 @@
-class TickTackToe{
-	char field[3][3];
+cclass TickTackToe {
+	Symbol field[3][3];
 	Player players[2];
 public:
 	TickTackToe() {
 		for (int i = 0; i < 3; i++)
 			for (int j = 0; j < 3; j++)
-				field[3][3] = '_';
-		players[2] = ('o'), ('x');
+				field[3][3] = EMPTY;
+		players[2] = (CIRCLE), (CROSS);
 	}
 
-	char * GetField()
-	{
+	Symbol * GetField() {
 		return *field;
 	}
-	
-	int CheckVictory(){
+
+	Symbol IfVictory() {
 		for (int i = 0; i < 3; i++) {
-			if (field[i][0] != '_') {
+			if (field[i][0] != EMPTY)
 				for (int j = 1; j < 2; j++)
 					if (field[i][0] != field[i][j] || field[0][i] != field[j][j])
 						break;
 					else if (j = 2)
-						return true;
-			}
+						return field[i][0];
 		}
 	}
 
-	char operator()(int x[2], int o[2]){
-			players[0].MakeTurn(field[x[0]][x[1]]);
-			players[1].MakeTurn(field[o[0]][o[1]]);
+	bool IfDraw() {
+		for (int i = 0; i < 3; i++)
+			for (int j = 1; j < 3; j++)
+				if (!field[i][j])
+					return false;
+		return true;
+	}
+
+	Symbol operator()(int x[2], int o[2]) {
+		players[0].MakeTurn(field[x[0]][x[1]]);
+		players[1].MakeTurn(field[o[0]][o[1]]);
 	}
 };
+
+	
