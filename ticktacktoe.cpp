ee6552eadf2f0845b6e2d7d604c92f6112d48267
Gemmunard <34251020+Gemmunard@users.noreply.github.com>
2018-09-05 14:37:35 +0300

    Update ticktacktoe.cpp

--- ticktacktoe.cpp
+++ ticktacktoe.cpp
@@ -15,16 +15,15 @@ public:
 	}
 	
 	int CheckVictory(){
-		if ((field[0][0] == field[1][1] == field[2][2] || field[2][0] == field[1][1] == field[0][2]) == ('x' || 'y') )
-			return true;
-		for (int i = 0; i < 3; i++)
-			if(( field[i][0] == field[i][1] == field[i][2] || field[0][i] == field[1][i] == field[2][i] ) == ('x' || 'y') )
-				return true;
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
-				if(field[i][j] == '_')
-					return false;
-		return 3;
+		for (int i = 0; i < 3; i++) {
+			if (field[i][0] != '_') {
+				for (int j = 1; j < 2; j++)
+					if (field[i][0] != field[i][j] || field[0][i] != field[j][j])
+						break;
+					else if (j = 2)
+						return true;
+			}
+		}
 	}
 
 	char operator()(int x[2], int o[2]){
